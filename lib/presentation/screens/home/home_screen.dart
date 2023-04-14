86e0dc04f982ea50503ecc7d5c605415ba93519a
dr.m <dr.glasdou@gmail.com>
2023-04-13 19:46:11 -0600

    done: basic routes

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -42,7 +43,15 @@ class _CustomListTile extends StatelessWidget {
       trailing: const Icon(Icons.arrow_forward_ios_rounded),
       title: Text(item.title),
       subtitle: Text(item.subTitle),
-      onTap: () {},
+      onTap: () {
+        // Navigator.of(context).push(
+        //   MaterialPageRoute(
+        //     builder: (context) => const ButtonsScreen(),
+        //   ),
+        // );
+
+        Navigator.pushNamed(context, item.link);
+      },
     );
   }
 }
